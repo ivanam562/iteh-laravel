0c172a29d47841b07bfcb351866ff01f689cc7ac
ivanam562 <ivana.mijatovici302@gmail.com>
2024-12-04 09:00:52 +0100

    Routes for UserController created

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,19 @@ class UserController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        return 'Lista korisnika';
     }
+    public function show(User $user)
+    {
+        return 'Lista korisnika';
+    }
+    public function destroy($id)
+    {
+        if($user->id != auth()->user()->id && auth()->user()->isAdmin()) {
+            $user->delete();
+            return response()->json('User is deleted successfully.');
+        }
+        return response()->json('You do not have the privilege to delete user if you are not admin, nor to delete your own account.');
+    }
+
 }
